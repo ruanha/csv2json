4cd4f86ed1d91978f67eaff43d7a8e8963078ddb
ruanha <rune_hartvig@hotmail.com>
2018-10-19 09:31:50 +0200

    name change

--- test/test.js
+++ test/test.js
@@ -1,9 +1,15 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
-    });
-  });
-});
+/* eslint-disable */
+const assert = require('assert')
+const parser = require('../csvparser.js')
+
+describe('Import module', function() {
+  describe('import', function() {
+
+    //tests
+    it('is imported', function() {
+      assert(parser !== null)
+    })
+  })
+})
+
 
